9545ac9f377808edd3c604ce918cff887c85802e
Debby Ji <jasmine820921@gmail.com>
2021-08-22 22:52:29 +0800

    feat: demo

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Taskbox', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const taskboxElement = screen.getByText(/Taskbox/i);
+  expect(taskboxElement).toBeInTheDocument();
 });
